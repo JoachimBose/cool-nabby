8b0a21c78c9cfd6ba68fbeec2accd4a9346f992f
JoachimBose <joachimbose48@gmail.com>
2026-01-09 20:13:07 +0100

    pwm is working

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 source /esp/esp-idf/export.sh
+idf.py set-target esp32-c6
+idf.py fullclean
+bear -- idf.py build
 /bin/bash
--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -4,26 +4,67 @@
  * SPDX-License-Identifier: CC0-1.0
  */
 
-#include <stdio.h>
-#include <inttypes.h>
-#include "sdkconfig.h"
+#include "driver/gpio.h"
+#include "esp_err.h"
+#include "esp_system.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "esp_chip_info.h"
-#include "esp_flash.h"
-#include "esp_system.h"
+#include "hal/gpio_types.h"
+#include "hal/ledc_types.h"
+#include "soc/clk_tree_defs.h"
+#include "driver/ledc.h"
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 #define SOUND_HZ 1000
+#define GPIO_SOUND_PIN 16
+#define GPIO_LED_PIN 15
+#define SENSOR_PIN 23
 
-void app_main(void)
-{
-    printf("Hello Raj!\n");
+void do_pwm_stuff() {
+  // gpio_config_t gpio = {
+  //   .intr_type = GPIO_INTR_DISABLE,
+  //   .mode = G,
+  //   .pull_up_en = GPIO_PULLUP_DISABLE,
+  //   .pull_down_en = GPIO_PULLDOWN_DISABLE,
+  //   .pin_bit_mask = 1ULL << GPIO_LED_PIN
+  // };
+  // ESP_ERROR_CHECK(gpio_config(&gpio));
 
-    for (int i = 10; i >= 0; i--) {
-        printf("Restarting in %d seconds...\n", i);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-    printf("Restarting now.\n");
-    fflush(stdout);
-    esp_restart();
+  ledc_timer_config_t timer_cfg = {
+    .speed_mode = LEDC_LOW_SPEED_MODE,
+    .timer_num = LEDC_TIMER_0,
+    .freq_hz = SOUND_HZ,
+    .clk_cfg = LEDC_AUTO_CLK,
+    .duty_resolution = 10,
+  };
+  ESP_ERROR_CHECK(ledc_timer_config(&timer_cfg));
+
+  ledc_channel_config_t chanl_cfg = {
+    .gpio_num = GPIO_SOUND_PIN,
+    .speed_mode = LEDC_LOW_SPEED_MODE,
+    .channel = LEDC_CHANNEL_0,
+    .intr_type = LEDC_INTR_DISABLE,
+    .timer_sel = LEDC_TIMER_0,
+    .duty = 512,
+    .sleep_mode = LEDC_SLEEP_MODE_KEEP_ALIVE,
+  };
+  ESP_ERROR_CHECK(ledc_channel_config(&chanl_cfg));
+  
+  ESP_ERROR_CHECK(ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 512));
+  ESP_ERROR_CHECK(ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0));
+}
+
+void app_main(void) {
+  printf("Hello Raj! pwm2!\n");
+  do_pwm_stuff();
+
+  for (int i = 5; i >= 0; i--) {
+    printf("Restarting in %d seconds...\n", i);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+  printf("Restarting now.\n");
+  fflush(stdout);
+  esp_restart();
 }
